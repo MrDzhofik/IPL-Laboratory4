6149a4e3610e1cee829fdce8fe240455016abccf
MrDzhofik <djafarovemil04@mail.ru>
2022-09-29 16:12:13 +0300

    Add exceptions

--- JS/lr4.js
+++ JS/lr4.js
@@ -7,27 +7,35 @@ var flag = false;
 function read_data() {
     product_name = document.getElementById('name').value;
     product_price = document.getElementById('price').value;
-    document.getElementById('name').value = "";
-    document.getElementById('price').value = "";
+    if ((Number(product_price) == parseInt(product_price)) && (product_price > 0)) {
+        document.getElementById('name').value = "";
+        document.getElementById('price').value = "";
+        return true;
+    }
+    else {
+        alert("Only positive numbers!");
+    }
+    return false;
 }
 
 function add_data() {
-    id++;
-    read_data();
-    var tbody = document.getElementById("products").getElementsByTagName('tbody')[0];
-    var row = tbody.insertRow();
-    row.setAttribute("onMouseOver", "Mouse_Over(this)");
-    row.setAttribute("onMouseOut", "Mouse_Out(this)");
-    row.setAttribute("onClick", "add_to_bin(this)");
-    var id_cell = row.insertCell();
-    var name_cell = row.insertCell();
-    var price_cell = row.insertCell();
-    var p_id = document.createTextNode(id);
-    var p_name = document.createTextNode(product_name);
-    var p_price = document.createTextNode(product_price);
-    id_cell.appendChild(p_id);
-    name_cell.appendChild(p_name);
-    price_cell.appendChild(p_price);
+    if (read_data()) {
+        id++;
+        var tbody = document.getElementById("products").getElementsByTagName('tbody')[0];
+        var row = tbody.insertRow();
+        row.setAttribute("onMouseOver", "Mouse_Over(this)");
+        row.setAttribute("onMouseOut", "Mouse_Out(this)");
+        row.setAttribute("onClick", "add_to_bin(this)");
+        var id_cell = row.insertCell();
+        var name_cell = row.insertCell();
+        var price_cell = row.insertCell();
+        var p_id = document.createTextNode(id);
+        var p_name = document.createTextNode(product_name);
+        var p_price = document.createTextNode(product_price);
+        id_cell.appendChild(p_id);
+        name_cell.appendChild(p_name);
+        price_cell.appendChild(p_price);
+    };
 }
 
 function add_to_bin(elem) {
